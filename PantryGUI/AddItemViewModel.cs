e4c6dd0d40634ece92c92e10e4112e1e71493555
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-03-19 15:02:38 +0100

    added Icommand

---
+++ PantryGUI/AddItemViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Mvvm;
+using Prism.Commands;
+
+namespace PantryGUI.ViewModels
+{
+    public class AddItemViewModel : BindableBase
+    {
+        private string _name;
+        private int _quantity;
+        private string _category;
+        private ICommand _cancelCommand;
+
+        public ICommand CancelCommand
+        {
+            get
+            {
+                return _cancelCommand ??= (new DelegateCommand(CancelHandler));
+            }
+        }
+
+        private void CancelHandler()
+        {
+
+        }
+
+
+
+    }
+}
