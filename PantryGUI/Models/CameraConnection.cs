9959415b2b9e689f59a92885a715f94aecb922b6
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-03-20 14:09:03 +0100

    added function

--- PantryGUI/Models/CameraConnection.cs
+++ PantryGUI/Models/CameraConnection.cs
@@ -7,6 +7,17 @@ namespace PantryGUI.Models
 {
     public class CameraConnection
     {
+
+        //Lav et interfase til den 
+        //tilføj et event i interfaset:
+        //event BarcodeFound ......
+
+        //lav en eventargs klasse som indeholder en string
+        //handleren kan så implenteres i view modellen
+
+        //Den skal have on /off funktion
+
+
         public string GetBarcode()
         {
             return "barcode";
