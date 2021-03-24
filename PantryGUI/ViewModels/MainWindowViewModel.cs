4dbbab5696abab2fdc25b9c4e50edae4904e6888
KongKeano <keanhansen@hotmail.com>
2021-03-24 14:58:46 +0100

    MainWindow første udkast

---
+++ PantryGUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using PantryGUI.Models;
+using Prism.Commands;
+
+
+namespace PantryGUI.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public tempUser t1
+        {
+            get;
+            set;
+        }
+        
+        public MainWindowViewModel()
+        {
+            t1 = new tempUser("Kristian");
+        }
+
+        ICommand _addItemCommand;
+
+        public ICommand AddItemCommand
+        {
+            get { return _addItemCommand ?? (_addItemCommand = new DelegateCommand(AddItemExecute)); }
+        }
+
+        void AddItemExecute()
+        {
+            MessageBox.Show("This is where u add an item...");
+        }
+
+
+    }
+}
