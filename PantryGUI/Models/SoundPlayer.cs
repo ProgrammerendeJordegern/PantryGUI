23ce424aaadfa3657c3596aa6127489a6b79d122
Simon <simonkjaer449@gmail.com>
2021-03-24 13:29:42 +0100

    Made SoundPlayer class with interface

---
+++ PantryGUI/Models/SoundPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PantryGUI.Models
+{
+    public class SoundPlayer : ISoundPlayer
+    {
+        public SoundPlayer()
+        {
+            Mute = false;
+        }
+
+        public bool Mute { get; set; }
+
+        public void Play()
+        {
+            if (Mute == false)
+            {
+                Console.Beep();
+            }
+        }
+    }
+}
